fd3c8209409ad14f272663d425567325ce657f1b
iamjyothika <jyothikajothu13@gmail.com>
2024-10-01 19:28:50 +0530

    models created

---
+++ jobapp/models.py
@@ -0,0 +1,67 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.core.exceptions import ValidationError
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('candidate', 'Candidate'),
+        ('employer', 'Employer'),
+        ('admin', 'Admin'),
+    ]
+
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return self.username
+
+class Company(models.Model):
+    companyname=models.CharField(max_length=100) 
+    location=models.CharField(max_length=100) 
+    description=models.TextField() 
+    owner=models.ForeignKey(User,on_delete=models.CASCADE)
+    def clean(self):
+        
+        if self.owner and self.owner.role != 'employer':
+            raise ValidationError("The owner must have the Employer role.")
+    
+    def __str__(self):
+        return self.name
+    
+class JobListing(models.Model):
+    title = models.CharField(max_length=200)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    description = models.TextField()
+    requirements = models.TextField()
+    location = models.CharField(max_length=100)
+    salary = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title  
+
+class JobApplication(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('accepted', 'Accepted'),
+        ('rejected', 'Rejected'),
+    ]
+    
+    job = models.ForeignKey(JobListing, on_delete=models.CASCADE)
+    candidate = models.ForeignKey(User, on_delete=models.CASCADE)
+    resume = models.FileField(upload_to='resumes/')
+    cover_letter = models.TextField(blank=True, null=True)  
+    applied_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+
+    def clean(self):
+        
+        if self.candidate and self.candidate.role != 'candidate':
+            raise ValidationError("Only users with the Candidate role can apply for jobs.")
+    
+    def __str__(self):
+        return f'{self.candidate.username} - {self.job.title}'      
+
